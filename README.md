4a964356c8ee0b1914ddbe5a8cff8dd8049d99ca
Ana Bollain <ana.bollain@gmail.com>
2023-01-18 14:10:33 +0100

    Upload project

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# javascript-personal-project-drum-kit
+# JS Drum Kit
+
+## How It Works
+
+Let's hit the drums! The interface is pretty simple, just press a key to hit the corresponding sound.
+
+## Description
+
+The aim of this project is to make a specific drum sound related to the pressed key. 
+
+It uses CSS transitions to obtain a more dynamic result and improve the user's experience. 
+
+The logic is based on using the data-key attribute as a hook to relate both the pressed key element and the audio element. An event listener is applied to the window, so that whenever the keyCode matches the data-key attribute, it will hit the sound and run the CSS transition. 
+
+## Javascript Used
+
+* querySelector()
+* addEventListener()
+* data attribute
+* window keyCode property
+* HTML audio properties and methods
+* forEach()
+* classList
+* CSS transitions
+* transitionend event
+
+## New things learned
+
+Retrieving elements from the DOM is not limited to just "id", "class" and elements. You can also select DOM elements using its attributes.
+
+When adding an event listener to the window, each key has a related number (keyCode) that we can use in Javascript for other actions. This website is quite useful to obtain each number: https://www.toptal.com/developers/keycode
+
+We can stop a CSS transition and remove a certain class from Javascript, with the 'transitionend' event and the event's propertyName value.
+
+The audio element has many properties and methods, such as currentTime or play().
+
+
+## Credits
+
+* [30 Day Vanilla JS Coding Challenge](https://javascript30.com/)
+
---
+++ main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+function removeTransition(ev) {
+    if(ev.propertyName !== 'transform') return;
+    this.classList.remove('js-play');
+}
+
+function addRemoveClass(element){
+    //Add CSS class
+    element.classList.add('js-play');
+    //Select all key elements 
+    const keysEl = document.querySelectorAll('.key');
+    //Transition end ev for the added CSS class 
+    keysEl.forEach(key => key.addEventListener('transitionend', removeTransition));
+}
+
+function playSound(ev) {
+    const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
+    //Stop function from running all together if audio equal to null
+    if(!audio) return; 
+    //Rewind audio to the start each time user presses key
+    audio.currentTime = 0; 
+    audio.play();
+    addRemoveClass(key);
+}
+
+window.addEventListener("keydown", playSound);
+
+
+//Personal solution
+//Variables
+/*const keysEl = document.querySelectorAll('.key');
+const audiosEl = document.querySelectorAll('.audio');
+
+function playSound(ev) {
+    //First loop through key elements
+    for(const key of keysEl){
+        //Second loop through audio elements
+        for(const audio of audiosEl){
+            if(ev.keyCode === parseInt(key.dataset.key) && ev.keyCode === parseInt(audio.dataset.key)){
+                //Play corresponding audio element with the same data-key
+                audio.currentTime = 0; 
+                audio.play();
+                //Add CSS class to highlight pressed key
+                key.classList.add('js-play');
+                //Remove class after a certain time
+                setTimeout(removeClass, 500);
+                //Function to remove class from the pressed key
+                function removeClass() {
+                    key.classList.remove('js-play');
+                }
+                }
+        }
+    }
+}
+
+window.addEventListener("keydown", playSound);
+*/
